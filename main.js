5bd90bb9c1591dd6faf9df3c856a05e73900568e
azernov <azernov@incom-sr.ru>
2024-02-19 08:58:56 +0300

    Урок 12: поддержка JSX синтаксиса

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-// опция as позволяет получить содержимое модуля в виде строки
-// опция url позволяет получить вместо содержимого модуля путь к файлу
+import { template } from './src/12.jsx';
 
-/* const modules = import.meta.glob('./src/10/*.js', {
-    //as: 'raw', // получим строковое предстваление каждого модуля
-    as: 'url', // получим путь до модуля
-    eager: true 
-}); */
-
-// можно указать какие данные мы хотим получить
-/* const modules = import.meta.glob('./src/10/*.js', {
-    import: 'name',
-    //import: 'default',
-}); */
-
-// в этом случае объект modules будет содержать экспорт из модулей
-/* console.log(modules);
-
-document.addEventListener('click', () => {
-    Object.values(modules).forEach((module) => {
-        module()
-            .then((data) => {
-                console.log(data);
-            })
-    });
-}); */
-
-// в этом случае мы получим экспорт сразу
-const modules = import.meta.glob('./src/10/*.js', {
-    import: 'name',
-    eager: true,
-});
-
-console.log(modules);
+document.querySelector('#app').appendChild(template);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import Inspect from 'vite-plugin-inspect';
 
 export default {
+    esbuild: {
+        // указываем имя функции, которая должна быть вызвана для обработки
+        // каждого JSX элемента
+        jsxFactory: 'create',
+        // указываем содержимое, которое автоматически будет вставлено в jsx-файлы
+        jsxInject: 'import { create } from "/src/12-create.js"',
+    },
     // сменить название папки public
     //publicDir: 'build',
     plugins: [
